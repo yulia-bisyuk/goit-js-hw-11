42bc1a3d0bfbfec8debd39a586360516b1c95eec
yulia-bisyuk <yulia.bisyuk@gmail.com>
2022-01-20 13:32:13 +0200

    task finished

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import bootstrap from 'bootstrap';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
+import photoTemplate from '../src/templates/photos-tmpl.hbs';
 
 const queryString = require('query-string');
+const Handlebars = require("handlebars");
 const refs = {
     gallery: document.querySelector('.gallery'),
     input: document.querySelector('input'),
@@ -20,14 +21,13 @@ refs.loadMoreBtn.addEventListener('click', onSearchSubmit);
 
 let lightbox = new SimpleLightbox('.gallery a');
 let currentPage = 1;
-let hitsPerPage = 40;
+let hitsPerPage = 100;
 
 async function fetchPictures() {
 
     const API_KEY = '25297171-b070f342ccd33435260198644';
     axios.defaults.baseURL = 'https://pixabay.com/api/';
   
-
     const params = {
         q: refs.input.value,
         image_type: 'photo',
@@ -37,22 +37,16 @@ async function fetchPictures() {
         per_page: hitsPerPage,
     };
     const searchParams = queryString.stringify(params);
-    console.log(searchParams);
 
     if (!refs.input.value) return;
 
     try {
         const response = await axios.get(`?key=${API_KEY}&${searchParams}`);
-        // console.log(data);
         const pictures = await response.data;
-
-        console.log(pictures);
-
-        // do { Notiflix.Notify.info(`Hooray! We found ${pictures.totalHits} images.`);
-        // } while (currentPage === 1)
         
             currentPage +=1;
         if (pictures.hits.length === 0) {
+            refs.loadMoreBtn.classList.add('visually-hidden');
             Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
         }
         else if ((hitsPerPage * currentPage) > pictures.totalHits) {
@@ -92,16 +86,15 @@ function infinityScroll() {
     }
 
     const handleObserver = ([item]) => {
-        // console.log('item intersecting:', item.isIntersecting);
+       
         if (item.isIntersecting) {
-            // console.log('item intersecting:', item.isIntersecting);
             loadMore();
-
         }
     }
     const observer = new IntersectionObserver(handleObserver, options);
     observer.observe(refs.loadMoreBtn);
 }
+
 infinityScroll();
 
 function loadMore() {
@@ -112,38 +105,9 @@ function loadMore() {
 
 function renderMarkup(pictures) {
 
-   const markup = pictures.hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-
-        return `
-        <div class="img-thumb">
-        <a href="${largeImageURL}" class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" width="370px" height="294"/>
-  </a>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes:</b>
-      <br> ${likes}
-    </p>
-    <p class="info-item">
-      <b>Views:</b>
-      <br> ${views}
-    </p>
-    <p class="info-item">
-      <b>Comments:</b>
-      <br> ${comments}
-    </p>
-    <p class="info-item">
-      <b >Downloads:</b>
-      <br> ${downloads}
-    </p>
-  </div>
-  </div>
-`;
-    }).join('');
-   
+    const markup = photoTemplate(pictures.hits);
     refs.gallery.insertAdjacentHTML('beforeend', markup);
     lightbox.refresh();
-    
     
 }
 
